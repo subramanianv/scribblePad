87b9d4bed73ad1211a7d2ac5f1b28332c7a97dd0
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2014-02-02 03:16:37 -0500

    Update scribbleServer.js

--- scribbleServer.js
+++ scribbleServer.js
@@ -27,22 +27,3 @@ io.sockets.on('connection',function(socket) {
      socket.broadcast.emit('imgDataFromOtherClients',imgData);
   });
 });
-//var md5sum = crypto.createHash('md5');
-//md5sum.update(getDateTime());
-//console.log(md5sum.digest('hex'));
-function getDateTime() {
-    var date = new Date();
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
-}
-
